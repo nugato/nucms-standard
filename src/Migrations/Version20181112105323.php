0d84151f5bc6b17bd4ce2f026c98c6ad16b06da5
Jacek Bednarek <jbednarek@nugato.pl>
2018-11-12 11:56:26 +0100

    Upgrate to nucms v0.1.1 and change yml to yaml

---
+++ src/Migrations/Version20181112105323.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181112105323 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE nucms_taxon ADD image INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE nucms_taxon ADD CONSTRAINT FK_EA58EE27C53D045F FOREIGN KEY (image) REFERENCES nucms_file (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_EA58EE27C53D045F ON nucms_taxon (image)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE nucms_taxon DROP FOREIGN KEY FK_EA58EE27C53D045F');
+        $this->addSql('DROP INDEX IDX_EA58EE27C53D045F ON nucms_taxon');
+        $this->addSql('ALTER TABLE nucms_taxon DROP image');
+    }
+}
